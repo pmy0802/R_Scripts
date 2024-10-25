aff5743e40ee819e4ebb19e524dc7c1202235c4b
pmy0802 <127711730+pmy0802@users.noreply.github.com>
2024-10-25 11:44:35 +0900

    6장 행정경계지도와 지진분포 출력 + 7장 웹스크래핑

--- 1018.R
+++ 1018.R
@@ -82,7 +82,7 @@ ggplot(data = df_map) +
 install.packages("openxlsx")
 library(openxlsx)
 
-df = read.xlsx("D:/RProject/map_datasets/국내지진목록.xlsx")
+df = read.xlsx("D:/RProject/map_datasets/국내지진목록.xlsx", sheet=1, startRow = 4, colNames = FALSE)
 head(df)
 
 # X8열에서 북한으로 시작하는 데이터의 행번호 추출
@@ -90,8 +90,9 @@ idx = grep("^북한", df$X8)
 # 북한지역의 X8열의 데이터를 확인
 df[idx, 'X8']
 # X8열에 북한으로 시작하는 행삭제
-df = df[-idx]
+df = df[-idx,]
 
+df
 # df에 있는 6열과 7열의 데이터 중 N과 E를 삭제하는 방법
 df[, 6] = gsub("N", "", df[,6])
 df[, 7] = gsub("E", "", df[,7])
@@ -101,6 +102,34 @@ df[, 6] = as.numeric(df[, 6])
 df[, 7] = as.numeric(df[, 7])
 
 df[,6]
+
+# 행정경계지도와 지진분표 출력
+# shapefile 읽어오기
+
+map = st_read("D:/RProject/map_datasets/Z_NGII_N3A_G0010000.shp")
+
+# WGS84 좌표계로 변환(지도출력)
+map = st_transform(map, crs=4326)
+
+# 포인트 데이터를 sf객체로 변환(포인트 출력)
+df_sf = df%>%st_as_sf(coords = c("X7", "X6"), crs=4326)
+
+# 행정경계지도 출력
+ggplot() +
+  geom_sf(data = map, fill="white", alpha=0.5, color="black") +
+  geom_sf(data = df_sf, aes(size=X3), shape=21, fill="red", alpha=0.3, color="black") + 
+  theme(legend.position = "none") +
+  labs(title = "지진분포", x="경도", y="위도")
+
+
+
+
+
+
+
+
+
+
 
 
 
